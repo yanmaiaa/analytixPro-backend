55a97203d70b96a1d394f6b9dfb1f1c82b76b9dc
Yan Maia <yandamasceno01@gmail.com>
2025-03-29 23:27:05 -0300

    estrutura inicial da aplicação

---
+++ src/main/java/proj_analytix/analytix/AnalytixProApplication.java
@@ -0,0 +1,13 @@
+package proj_analytix.analytix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalytixProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalytixProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj_analytix/analytix/AnalytixProApplicationTests.java
@@ -0,0 +1,13 @@
+package proj_analytix.analytix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalytixProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
